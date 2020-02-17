ea68959b9be5299233eb270c017eadb7c8f38e1d
Educorreia932 <skelozard1@gmail.com>
2020-02-17 17:53:25 +0000

    Moved the terminal initialization to a new class Game and added draw() and run() methods

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,28 +1,5 @@
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
-import java.io.IOException;
-
 public class Application {
     public static void main(String[] args) {
-        try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
 
-            screen.setCursorPosition(null);   // we don't need a cursor
-            screen.startScreen();             // screens must be started
-            screen.doResizeIfNecessary();     // resize screen if necessary
-
-            screen.clear();
-            screen.setCharacter(10, 10, new TextCharacter('X'));
-            screen.refresh();
-        }
-
-        catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
---
+++ src/main/java/Game.java
@@ -0,0 +1,36 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    public Game() {
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+
+            screen.clear();
+            screen.setCharacter(10, 10, new TextCharacter('X'));
+            screen.refresh();
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() {
+
+    }
+
+    private void run() {
+        
+    }
+}
