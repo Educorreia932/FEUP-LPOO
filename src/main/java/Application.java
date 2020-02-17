364688c6c8a4fd73ab12da1cfc809dbc194ad17b
Educorreia932 <skelozard1@gmail.com>
2020-02-17 17:47:49 +0000

    Added a terminal to the application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,23 @@
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
