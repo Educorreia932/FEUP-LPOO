0f3c49b48b4f761b8e44f8fc6ecc14cad14876fd
Educorreia932 <skelozard1@gmail.com>
2020-04-19 14:39:23 +0100

    Wrote tests for PokemonType class

--- src/main/java/lpoo/pokemonascii/pokemon/PokemonType.java
+++ src/main/java/lpoo/pokemonascii/pokemon/PokemonType.java
@@ -4,8 +4,8 @@ enum Type {
     NORMAL,
     FIRE,
     WATER,
-    GRASS,
     ELECTRIC,
+    GRASS,
     ICE,
     FIGHTING,
     POISON,
@@ -20,28 +20,47 @@ enum Type {
     STEEL;
 }
 
-public abstract class PokemonType {
-    private final float[][] effectivenessTable = new float[][]{
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.5f, 0, 1, 1, 0.5f},
-        {1, 0.5f, 0.5f, 2, 1, 2, 1, 1, 1, 1, 1, 2, 0.5f, 1, 0.5f, 1, 2},
-        {1, 2, 0.5f, 0.5f, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 0.5f, 1, 1},
-        {1, 0.5f, 2, 0.5f, 1, 1, 1, 0.5f, 2, 0.5f, 1, 0.5f, 2, 1, 0.5f, 1, 0.5f},
-        {1, 1, 2, 0.5f, 0.5f, 1, 1, 1, 0, 2, 1, 1, 1, 1, 0.5f, 1, 1},
-        {1, 0.5f, 0.5f, 2, 1, 0.5f, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 0.5f},
-        {2, 1, 1, 1, 1, 2, 1, 0.5f, 1, 0.5f, 0.5f, 0.5f, 2, 0, 1, 2, 2},
-        {1, 1, 1, 2, 1, 1, 1, 0.5f, 0.5f, 1, 1, 1, 0.5f, 0.5f, 1, 1, 0},
-        {1, 2, 1, 0.5f, 2, 1, 1, 2, 1, 0, 1, 0.5f, 2, 1, 1, 1, 2},
-        {1, 1, 1, 2, 0.5f, 1, 2, 1, 1, 1, 1, 2, 0.5f, 1, 1, 1, 0.5f},
-        {1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 0.5f, 1, 1, 1, 1, 0, 0.5f},
-        {1, 0.5f, 1, 2, 1, 1, 0.5f, 0.5f, 1, 0.5f, 2, 1, 1, 0.5f, 1, 2, 0.5f},
-        {1, 2, 1, 1, 1, 2, 0.5f, 1, 0.5f, 2, 1, 2, 1, 1, 1, 1, 0.5f},
-        {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 0.5f, 0.5f},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 0.5f},
-        {1, 1, 1, 1, 1, 1, 0.5f, 1, 1, 1, 2, 1, 1, 2, 1, 0.5f, 0.5f},
-        {1, 0.5f, 0.5f, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 0.5f}
+public class PokemonType {
+    private static final float Z = 0.0f;
+    private static final float W = 0.5f;
+    private static final float N = 1.0f;
+    private static final float S = 2.0f;
+
+    private static final float[][] effectivenessTable = new float[][]{
+        {N, N, N, N, N, N, N, N, N, N, N, N, W, Z, N, N, W}, // Normal
+        {N, W, W, N, S, S, N, N, N, N, N, S, W, N, W, N, S}, // Fire
+        {N, S, W, N, W, N, N, N, S, N, N, N, S, N, W, N, N}, // Water
+        {N, N, S, W, W, N, N, N, Z, S, N, N, N, N, W, N, N}, // Electric
+        {N, W, S, N, W, N, N, W, S, W, N, W, S, N, W, N, W}, // Grass
+        {N, W, W, N, S, W, N, N, S, S, N, N, N, N, S, N, W}, // Ice
+        {S, N, N, N, N, S, N, W, N, W, W, W, S, Z, N, S, S}, // Fighting
+        {N, N, N, N, S, N, N, W, W, N, N, N, W, W, N, N, Z}, // Poison
+        {N, S, N, S, W, N, N, S, N, Z, N, W, S, N, N, N, S}, // Ground
+        {N, N, N, W, S, N, S, N, N, N, N, S, W, N, N, N, W}, // Flying
+        {N, N, N, N, N, N, S, S, N, N, W, N, N, N, N, Z, W}, // Psychic
+        {N, W, N, N, S, N, W, W, N, W, S, N, N, W, N, S, W}, // Bug
+        {N, S, N, N, N, S, W, N, W, S, N, S, N, N, N, N, W}, // Rock
+        {Z, N, N, N, N, N, N, N, N, N, S, N, N, S, N, W, N}, // Ghost
+        {N, N, N, N, N, N, N, N, N, N, N, N, N, N, S, N, W}, // Dragon
+        {N, N, N, N, N, N, W, N, N, N, S, N, N, S, N, W, N}, // Dark
+        {N, W, W, W, N, S, N, N, N, N, N, N, S, N, N, N, W}  // Steel
     };
 
-    public float getDamageMultiplier(Type attackType, Type defensiveType) {
-        return effectivenessTable[attackType.ordinal()][defensiveType.ordinal()];
+    private Type type;
+
+    public PokemonType(String type) {
+        this.type = Type.valueOf(type);
+    }
+
+    public float getDamageMultiplier(Type defensiveType) {
+        return effectivenessTable[type.ordinal()][defensiveType.ordinal()];
+    }
+
+    public float getDamageMultiplier(PokemonType defensiveType) {
+        return effectivenessTable[type.ordinal()][defensiveType.getType().ordinal()];
+    }
+
+    public Type getType() {
+        return type;
     }
 }
--- src/test/java/lpoo/pokemonascii/pokemon/PokemonTypeTest.java
+++ src/test/java/lpoo/pokemonascii/pokemon/PokemonTypeTest.java
@@ -1,4 +1,106 @@
 package lpoo.pokemonascii.pokemon;
 
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class PokemonTypeTest {
+    private PokemonType pokemonType;
+
+    @Test
+    public void testNormalType() {
+        pokemonType = new PokemonType("NORMAL");
+        assertEquals(pokemonType.getDamageMultiplier(Type.ROCK), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GHOST), 0.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.STEEL), 0.5f);
+    }
+
+    @Test
+    public void testFireType() {
+        pokemonType = new PokemonType("FIRE");
+        assertEquals(pokemonType.getDamageMultiplier(Type.FIRE), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.WATER), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GRASS), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ICE), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.BUG), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ROCK), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.DRAGON), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.STEEL), 2.0f);
+    }
+
+    @Test
+    public void testWaterType() {
+        pokemonType = new PokemonType("WATER");
+        assertEquals(pokemonType.getDamageMultiplier(Type.FIRE), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.WATER), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GRASS), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GROUND), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ROCK), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.DRAGON), 0.5f);
+    }
+
+    @Test
+    public void testElectricType() {
+        pokemonType = new PokemonType("ELECTRIC");
+        assertEquals(pokemonType.getDamageMultiplier(Type.WATER), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ELECTRIC), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GRASS), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GROUND), 0.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.FLYING), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.DRAGON), 0.5f);
+    }
+
+    @Test
+    public void testGrassType() {
+        pokemonType = new PokemonType("GRASS");
+        assertEquals(pokemonType.getDamageMultiplier(Type.FIRE), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.WATER), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GRASS), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.POISON), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GROUND), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.FLYING), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.BUG), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ROCK), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.DRAGON), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.STEEL), 0.5f);
+    }
+
+    @Test
+    public void testIceType() {
+        pokemonType = new PokemonType("ICE");
+        assertEquals(pokemonType.getDamageMultiplier(Type.FIRE), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.WATER), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GRASS), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ICE), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GROUND), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.FLYING), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.DRAGON), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.STEEL), 0.5f);
+    }
+
+    @Test
+    public void testFightingType() {
+        pokemonType = new PokemonType("FIGHTING");
+        assertEquals(pokemonType.getDamageMultiplier(Type.NORMAL), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ICE), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.POISON), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.FLYING), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.PSYCHIC), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.BUG), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ROCK), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GHOST), 0.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.DARK), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.STEEL), 2.0f);
+    }
+
+    @Test
+    public void testPoisonType() {
+        pokemonType = new PokemonType("POISON");
+        assertEquals(pokemonType.getDamageMultiplier(Type.GRASS), 2.0f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.POISON), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GROUND), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.ROCK), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.GHOST), 0.5f);
+        assertEquals(pokemonType.getDamageMultiplier(Type.STEEL), 0.0f);
+    }
 }
