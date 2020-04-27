d8df1d95125e076e30507723154d77b1fc3d54a3
anaines14 <anaiobarros2014@gmail.com>
2020-04-27 17:04:15 +0100

    Test for sets and gets of state

--- src/test/java/lpoo/pokemonascii/PlayerTest.java
+++ src/test/java/lpoo/pokemonascii/PlayerTest.java
@@ -1,4 +1,33 @@
 package lpoo.pokemonascii;
 
+import lpoo.pokemonascii.data.Player;
+import lpoo.pokemonascii.data.Position;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlayerTest {
+    private Player player;
+
+    @Before
+    public void init(){
+        player = new Player();
+    }
+
+    @Test
+    public void testSetGetsStates(){
+        player.setState(Player.State.BACK);
+        assertEquals(Player.State.BACK, player.getState());
+
+        player.setState(Player.State.FRONT);
+        assertEquals(Player.State.FRONT, player.getState());
+
+        player.setState(Player.State.LEFT);
+        assertEquals(Player.State.LEFT, player.getState());
+
+        player.setState(Player.State.RIGHT);
+        assertEquals(Player.State.RIGHT, player.getState());
+    }
+
 }
