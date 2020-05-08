19655f0719174a818628b3206333bf3429486fb5
Educorreia932 <skelozard1@gmail.com>
2020-05-08 16:16:24 +0100

    Placed every summary element in place

--- src/main/java/lpoo/pokemonascii/gui/PokemonSummaryView.java
+++ src/main/java/lpoo/pokemonascii/gui/PokemonSummaryView.java
@@ -10,25 +10,34 @@ import java.io.IOException;
 
 public class PokemonSummaryView {
     private Screen screen;
+    private Sprite background;
+    private Sprite info;
+    private Sprite selectedBar;
     private TextGraphics graphics;
     private TextRenderer pokedexNumber;
     private TextRenderer pokemonSpecies;
     private TextRenderer pokemonName;
     private PokemonRenderer pokemon;
-    private Sprite background;
-    private Sprite info;
 
     public PokemonSummaryView(Screen screen, TextGraphics graphics, PokemonSummaryModel model) {
         this.screen = screen;
         this.graphics = graphics;
 
-        pokedexNumber = new TextRenderer(0, 0, String.valueOf(model.getPokemon().getPokedexNumber()));
-        pokemonSpecies = new TextRenderer(0, 0, model.getPokemon().getName());
-        pokemonName = new TextRenderer(0, 0, model.getPokemon().getName());
-        pokemon = new PokemonRenderer(50, 50, model.getPokemon());
+        background = new Sprite("summary\\pokemon_background");
+        info = new Sprite("summary\\pokemon_info");
+        selectedBar = new Sprite("summary\\selected_bar");
+
+        pokedexNumber = new TextRenderer(280, 23, String.valueOf(model.getPokemon().getPokedexNumber()));
+        pokemonSpecies = new TextRenderer(80, 21, model.getPokemon().getName());
+        pokemonName = new TextRenderer(280, 38, model.getPokemon().getName());
+        pokemon = new PokemonRenderer(50, 35, model.getPokemon());
     }
 
     public void draw() throws IOException {
+        Sprite.drawSprite(info, 0, 16, graphics);
+        Sprite.drawSprite(background, 0, 16, graphics);
+        Sprite.drawSprite(selectedBar, graphics);
+
         pokedexNumber.draw(graphics);
         pokemonSpecies.draw(graphics);
         pokemonName.draw(graphics);
